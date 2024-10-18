b6a7c22670c214c916561907c14fabe0dcb7680e
Greed_41 <103091733+dirga41@users.noreply.github.com>
2024-10-18 15:08:04 +0700

    Add files via upload

---
+++ design-in-rn/screen/index.js
@@ -0,0 +1,34 @@
+import LotsOfStyles from "./LotsOfStyles";
+import FixedDimensionsBasics from "./FixedDimensionsBasics";
+import FlexDimensionsBasics from "./FlexDimensionsBasics";
+import PercentageDimensionsBasics from "./PercentageDimensionBasics";
+import FlexBasic from "./FlexBasic";
+import FlexDirectionBasics from "./FlexDirectionBasics";
+import JustifyContentBasics from "./JustifyContentBasics";
+import AlignItemsLayout from "./AlignItemsLayout";
+import WidthHeightBasics from "./WidthHeightBasics";
+import PositionLayout from "./PositionLayout";
+import DisplayAnImageWithStyle from "./DisplayAnimations";
+import NativeBaseBasic from "./NativeBaseBasic";
+
+export {
+  LotsOfStyles,
+  FixedDimensionsBasics,
+  FlexDimensionsBasics,
+  PercentageDimensionsBasics,
+  FlexBasic,
+  FlexDirectionBasics,
+  JustifyContentBasics,
+  AlignItemsLayout,
+  WidthHeightBasics,
+  PositionLayout,
+  DisplayAnimations,
+  NativeBaseBasic,
+};
+
+
+import Button from "./Button";
+import Header from "./Header";
+import Separator from "./Separator";
+
+export { Button, Header, Separator };
